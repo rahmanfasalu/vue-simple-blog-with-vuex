4a8794ba4dd5fbb1c46ec1c4d82caffe142d57c4
Fasalu Rahman <fasalukm@gmail.com>
2020-06-20 17:34:30 +0200

    Deploy

--- src/shared/config.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = process.env.VUE_APP_DATA_API;
-export const API_URL = {
-  getData: BASE_URL + "/data/data.json",
-};
--- src/shared/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./config";
-export * from "./dataService";
-export * from "./constants";
